5c2e84c452d7d8346709bc7c6ab6b7edb93983a3
inam000 <enamulhaq243@gmail.com>
2022-02-17 14:19:07 +0600

    erorr done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
 document.getElementById("Calculate-btn").addEventListener("click", function () {
 
+
+
     const food = document.getElementById("food-cost").value;
     const rent = document.getElementById("rent-cost").value;
-    const Clothes = document.getElementById("Clothes-cost").value;
+    const clothes = document.getElementById("clothes-cost").value;
+
+    if (!isNaN(food) && !isNaN(rent) && !isNaN(clothes)) {
+
+        const total = parseFloat(food) + parseFloat(rent) + parseFloat(clothes);
+
+        document.getElementById("total-expenses").innerText = total;
+        const income = document.getElementById("income").value;
+        const balance = document.getElementById("balance").innerText = income - total;
+        balance = "";
+    }
+
+    if (food > 0 && rent > 0 && clothes) {
+        const total = parseFloat(food) + parseFloat(rent) + parseFloat(clothes);
+
+        document.getElementById("total-expenses").innerText = total;
+        const income = document.getElementById("income").value;
+        const balance = document.getElementById("balance").innerText = income - total;
+
+    }
 
 
+    else {
+        alert("Enter Type Number or Possitive Number")
 
-    const total = parseFloat(food) + parseFloat(rent) + parseFloat(Clothes)
+    }
 
-
-    let expenses = document.getElementById("total-expenses")
-    expenses.innerText = total
-
-    const income = document.getElementById("income").value;
-    const balance = document.getElementById("balance")
-    balance.innerText = income - total
 
 
 
